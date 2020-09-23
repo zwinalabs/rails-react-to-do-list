e608f40f8415be90525c40f7b0bd6e58b15ae9f6
KAIS CHRIF <kais.chrif.dev@gmail.com>
2020-09-23 20:11:25 +0100

    Build a Header

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users
-  root 'pages#home'
-  get 'pages/my_todo_items'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  devise_for :users
+
+  root 'pages#home'
+
+  get 'pages/my_todo_items'
 end
