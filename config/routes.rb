ebbb01c261452a936b432879bd8608f57ff55afe
KAIS CHRIF <kais.chrif.dev@gmail.com>
2020-09-23 20:16:07 +0100

    Create Homepage for Authorized Users

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,10 @@ Rails.application.routes.draw do
 
   devise_for :users
 
+  authenticated :user do
+    root "pages#my_todo_items", as: :authenticated_root
+  end
+
   root 'pages#home'
 
-  get 'pages/my_todo_items'
 end
