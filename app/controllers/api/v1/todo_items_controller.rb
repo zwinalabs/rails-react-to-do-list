88527731bc7a3bc7658231c203cd6e092362cd04
KAIS CHRIF <kais.chrif.dev@gmail.com>
2020-09-24 06:18:07 +0100

    Create the API: Generate a Controller, Create Non Authorized End Points and API routes

---
+++ app/controllers/api/v1/todo_items_controller.rb
@@ -0,0 +1,17 @@
+class Api::V1::TodoItemsController < ApplicationController
+    before_action :set_todo_item, only: [:show, :edit, :update, :destroy]
+    def index
+    end
+    def show
+    end
+    def create
+    end
+    def update
+    end
+    def destroy
+    end
+    private
+        def set_todo_item
+            @todo_item = TodoItem.find(params[:id])
+        end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,11 @@ Rails.application.routes.draw do
   end
 
   root 'pages#home'
+  
+  namespace :api, defaults: { format: :json } do
+    namespace :v1 do
+      resources :todo_items, only: [:index, :show, :create, :update, :destroy]
+    end
+  end
 
 end
